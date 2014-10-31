c99bf6beb6e0031627e0062d72c8a886ee0dbd49
Kaiyin Zhong <kindlychung@gmail.com>
2014-10-31 16:39:02 +0100

    scripts path depends on os

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 import platform
 sysname = platform.system()
 if sysname == "Windows":
-    binpath = r"C:\Program Files"
+    binpath = r"%SystemRoot%"
 else:
     binpath = r"/usr/local/bin"
 
@@ -12,7 +12,7 @@ setup(
     packages = ["checkbed"],
     # scripts = ["scripts/checkcoll.py", "scripts/bedinfo.py"],
     data_files = [(binpath, ["scripts/bedinfo", "scripts/checkcoll"])],
-    version = "0.2.6",
+    version = "0.2.7",
     description = "A class for checking plink bed files",
     author = "Kaiyin Zhong",
     author_email = "kindlychung@gmail.com",
