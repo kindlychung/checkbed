c2c72407ba1e7c6706aca11c05e4624971049d79
Kaiyin Zhong <kindlychung@gmail.com>
2014-10-31 19:51:53 +0100

    systemroot is a env var in windows

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
-import platform
+import platform, os
 sysname = platform.system()
 if sysname == "Windows":
-    binpath = r"%SystemRoot%"
+    binpath = os.getenv("SystemRoot")
 else:
     binpath = r"/usr/local/bin"
 
@@ -12,7 +12,7 @@ setup(
     packages = ["checkbed"],
     # scripts = ["scripts/checkcoll.py", "scripts/bedinfo.py"],
     data_files = [(binpath, ["scripts/bedinfo", "scripts/checkcoll"])],
-    version = "0.2.7",
+    version = "0.2.8",
     description = "A class for checking plink bed files",
     author = "Kaiyin Zhong",
     author_email = "kindlychung@gmail.com",
